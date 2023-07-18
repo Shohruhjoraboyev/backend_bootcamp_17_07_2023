81de283f37ded9ef32fdfe9615d097bddb8be4ae
jonikramov <azizjonikramov1@gmail.com>
2023-07-18 13:25:14 +0500

    task sum of max & min

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // Task #1
 func oddOrEven(num int) string {
@@ -21,12 +23,45 @@ func swapNums(num1, num2 *int) {
 
 }
 
+// Task #3
+func sumMaxMin(a, b, c float64) float64 {
+	var max float64
+	var min float64
+
+	//find max
+	max = a
+
+	if b > max {
+		max = b
+	}
+
+	if c > max {
+		max = c
+	}
+
+	// find min
+	min = a
+
+	if b < min {
+		min = b
+	}
+
+	if c < min {
+		min = c
+	}
+
+	return max + min
+
+}
+
 func main() {
 	fmt.Println("Hello Go")
 
+	// Task #1
 	fmt.Println(oddOrEven(4)) // Even
 	fmt.Println(oddOrEven(5)) // Odd
 
+	// Task #2
 	num1 := 3
 	num2 := 5
 
@@ -37,5 +72,9 @@ func main() {
 
 	fmt.Println(num1)
 	fmt.Println(num2)
+
+	// Task #3
+	fmt.Println(sumMaxMin(1, 3, 2)) // 4
+	fmt.Println(sumMaxMin(4, 5, 6)) // 10
 
 }
