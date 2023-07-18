9db767a488b34a8ba1ab1d02d8df5188c425db3b
jonikramov <azizjonikramov1@gmail.com>
2023-07-18 11:47:42 +0500

    swap numbers

--- main.go
+++ main.go
@@ -12,10 +12,30 @@ func oddOrEven(num int) string {
 
 }
 
+// Task #2
+func swapNums(num1, num2 *int) {
+	var num3 int
+	num3 = *num1
+	*num1 = *num2
+	*num2 = num3
+
+}
+
 func main() {
 	fmt.Println("Hello Go")
 
 	fmt.Println(oddOrEven(4)) // Even
 	fmt.Println(oddOrEven(5)) // Odd
+
+	num1 := 3
+	num2 := 5
+
+	fmt.Println(num1)
+	fmt.Println(num2)
+
+	swapNums(&num1, &num2)
+
+	fmt.Println(num1)
+	fmt.Println(num2)
 
 }
