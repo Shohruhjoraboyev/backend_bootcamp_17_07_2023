00dc22838a0d0e2d1091bfcc2c727854dfa9bccd
jaloldinov <omadbekclever2@gmail.com>
2023-07-26 14:53:05 +0500

    leetcode tasks are completed

---
+++ lesson_4/decode.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	key := "the quick brown fox jumps over the lazy dog"
+	message := "vkbs bs t suepuv"
+
+	fmt.Println(decodeMessage(key, message)) // Output: "this is a secret"
+}
+
+func decodeMessage(key string, message string) string {
+	return "s"
+}
