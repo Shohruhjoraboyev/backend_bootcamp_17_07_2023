f5a796c345335c0c44f6455087e7b437b93a2353
jaloldinov <omadbekclever2@gmail.com>
2023-07-28 15:55:27 +0500

    palindrom array

--- lesson_6/palindrom.go
+++ lesson_6/palindrom.go
@@ -1,10 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
 func main() {
-	fmt.Println(strings.Count("cheese hele", "le"))
+	arr := []int{1, 2, 7, 2, 1} // true
+	// arr := []int{1, 4, 2, 1} // false
+
+	fmt.Println(chekPalindrom(arr))
+
+}
+
+func chekPalindrom(arr []int) bool {
+	for i := 0; i < len(arr)/2; i++ {
+		if arr[i] != arr[len(arr)-1-i] {
+			// fmt.Println(arr[i], arr[len(arr)-1-i])
+			return false
+		}
+	}
+	return true
 }
