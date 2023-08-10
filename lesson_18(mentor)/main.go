cf5eb170c76694635afb9e729c1280e3d8b555f8
jaloldinov <omadbekclever2@gmail.com>
2023-08-10 18:28:40 +0500

    7 th task is done

--- lesson_18(mentor)/main.go
+++ lesson_18(mentor)/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"task/fifth"
+	"task/seventh"
 )
 
 func main() {
@@ -22,7 +22,37 @@ func main() {
 	// fourth.TopTransactionCategory()
 	// fmt.Println()
 
-	fmt.Println("5.har bir branchda har bir categorydan qancha transaction bo'lgani")
-	fifth.TopBranchTransactionCategory()
+	// fmt.Println("5.har bir branchda har bir categorydan qancha transaction bo'lgani")
+	// fifth.TopBranchTransactionCategory()
+	// fmt.Println()
+
+	// fmt.Println("6. har bir branch nechta plus/minus transactionlar soni, plus/minus transactionlar summasini quyidagicha chiqarish")
+	// sixth.PlusMinus()
+	// fmt.Println()
+
+	fmt.Println("7. har bir kunda kirgan productlar sonini kamayish tartibida chiqarish")
+	seventh.CalculateProductIncome()
 	fmt.Println()
 }
+
+// 6. har bir branch nechta plus/minus transactionlar soni, plus/minus transactionlar summasini quyidagicha chiqarish:
+//                     Transactions            Summ
+//                     plus   minus        plus     minus
+//     1. Branch1      53      20          853 000  278 000
+//     2. Branch2      38      185         492 000  1 982 000
+
+// 7. har bir kunda kirgan productlar sonini kamayish tartibida chiqarish:
+//         kun         soni
+//     1. 2023-08-04   789
+//     2. 2023-08-12   634
+//     ...  ...  ...  ...
+// 8. Product qancha kiritilgan va chiqarilganligi jadvali:
+//     Name    Kiritilgan  Chiqarilgan
+//     Olma     345            847
+//     Cola     374            219
+//     ....     ...       ...   ....
+
+// 9. Filialda qancha summalik product borligi jadvali:
+
+// 1. Branch1        853 000
+// 2. Branch2      1 982 000
---
+++ lesson_19(assistant)/models/model.go
@@ -0,0 +1,45 @@
+package models
+
+type BranchTransactionCount struct {
+	BranchID   int
+	BranchName string
+	Count      int
+}
+type BranchTransactionCategoryCount struct {
+	BranchID     int
+	BranchName   string
+	CategoryName string
+	Count        int
+}
+type Transaction struct {
+	ID        int    `json:"id"`
+	BranchID  int    `json:"branch_id"`
+	ProductID int    `json:"product_id"`
+	Type      string `json:"type"`
+	Quantity  int    `json:"quantity"`
+	CreatedAt string `json:"created_at"`
+}
+type Branch struct {
+	ID     int    `json:"id"`
+	Name   string `json:"name"`
+	Adress string `json:"adress"`
+}
+
+type Products struct {
+	Id         int    `json:"id"`
+	Name       string `json:"name"`
+	Price      int    `json:"price"`
+	CategoryId int    `json:"category_id"`
+}
+
+type ProductTop struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Count int
+}
+
+type BranchProductPrice struct {
+	BranchID   int
+	BranchName string
+	Sum        int
+}
