b6f249f2fe2d0a63b833ec75fcb025f76f44da34
jaloldinov <omadbekclever2@gmail.com>
2023-08-25 16:49:05 +0500

    errer

--- month_1/lesson_21(jsontask)/main.go
+++ month_1/lesson_21(jsontask)/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	task2 "task/task_2"
+	task4 "task/task_4"
 )
 
 func main() {
@@ -10,17 +10,17 @@ func main() {
 	// task1.CalculateTranTopBranches()
 	// fmt.Println()
 
-	fmt.Println("2.transactionlar summasi bo'yicha top branches")
-	task2.CalculateSumOfPriceTopBranches()
-	fmt.Println()
+	// fmt.Println("2.transactionlar summasi bo'yicha top branches")
+	// task2.CalculateSumOfPriceTopBranches()
+	// fmt.Println()
 
 	// fmt.Println("3.transactionda bo'lgan top productlar")
 	// task3.TopTransactionProducts()
 	// fmt.Println()
 
-	// fmt.Println("4.transactionda bo'lgan top categorylar")
-	// task4.TopTransactionCategory()
-	// fmt.Println()
+	fmt.Println("4.transactionda bo'lgan top categorylar")
+	task4.TopTransactionCategory()
+	fmt.Println()
 
 	// fmt.Println("5.har bir branchda har bir categorydan qancha transaction bo'lgani")
 	// task5.TopBranchTransactionCategory()
