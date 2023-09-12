6b6acc4ff2e303ba41d807841dbeaa7401dc58e6
Sarvarbek Fozilov <123530266+jdu9a0009@users.noreply.github.com>
2023-09-12 16:24:19 +0500

    all done json task in postgressql

--- secondmonth/Lesson_5(Mentor)/postgres.sql
+++ secondmonth/Lesson_5(Mentor)/postgres.sql
@@ -143,4 +143,24 @@ FROM
 		1. Chilonzor      73         34
 		2. MGorkiy        60         75
 		...    ...     ...    ...       ...
-    */    
+    */  
+
+SELECT b.name, 
+	SUM(CASE WHEN bt.type='plus' THEN (bt.quantity) ELSE 0 END)/COUNT(bt.created_at) as "plus",
+	SUM(CASE WHEN bt.type='minus' THEN (bt.quantity) ELSE 0 END)/COUNT(bt.created_at) as "minus"
+	FROM branch b
+	JOIN branch_transaction bt ON bt.branch_id = b.id
+	GROUP BY b.name
+/*
+ 15. Har kuni o'rtacha user qancha summa product kiritgani va chiqargani bo'yicha jadval:
+     branch      o'rtacha+        o'rtacha-
+ 1. Anvar          370 000         435 000
+ 2. Shuhrat        60 000          875 000
+*/
+SELECT u.name, 
+	SUM(CASE WHEN bt.type='plus' THEN (bt.quantity*p.price) ELSE 0 END)/COUNT(bt.created_at) as "plus",
+	SUM(CASE WHEN bt.type='minus' THEN (bt.quantity*p.price) ELSE 0 END)/COUNT(bt.created_at) as "minus"
+	FROM "user" u
+	JOIN branch_transaction bt ON bt.user_id = u.id
+	JOIN product p ON p.id = bt.product_id
+	GROUP BY u.name  
