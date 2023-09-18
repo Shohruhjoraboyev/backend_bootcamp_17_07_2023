88c75245a3057aa0a2b1b5fe06a94572a5e7f087
jaloldinov <omadbekclever2@gmail.com>
2023-09-18 13:22:53 +0500

    tariff completed

--- month_2/lesson_18(mentor)/api/handler/tarfif.go
+++ month_2/lesson_18(mentor)/api/handler/tarfif.go
@@ -5,6 +5,7 @@ import (
 	"app/pkg/logger"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -40,14 +41,64 @@ func (h *Handler) GetStaffTarif(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "success", "data": resp})
 }
 
-func (h *Handler) UpdateStaffTarif(c *gin.Context) {
+func (h *Handler) GetAllStaffTarif(c *gin.Context) {
+	h.log.Info("request GetAllTariff")
+	page, err := strconv.Atoi(c.DefaultQuery("page", "1"))
+	if err != nil {
+		h.log.Error("error get page:", logger.Error(err))
+		c.JSON(http.StatusBadRequest, "invalid page param")
+		return
+	}
+	limit, err := strconv.Atoi(c.DefaultQuery("limit", "10"))
+	if err != nil {
+		h.log.Error("error get limit:", logger.Error(err))
+		c.JSON(http.StatusBadRequest, "invalid page param")
+		return
+	}
 
+	resp, err := h.storage.Tariff().GetAllStaffTarif(&models.GetAllStaffTarifRequest{
+		Page:  page,
+		Limit: limit,
+		Name:  c.Query("search"),
+	})
+	if err != nil {
+		h.log.Error("error Branch GetAllTariff:", logger.Error(err))
+		c.JSON(http.StatusInternalServerError, "internal server error")
+		return
+	}
+	h.log.Warn("response to GetAllTariff")
+	c.JSON(http.StatusOK, resp)
 }
 
-func (h *Handler) GetAllStaffTarif(c *gin.Context) {
+func (h *Handler) UpdateStaffTarif(c *gin.Context) {
+	var tariff models.StaffTarif
+	err := c.ShouldBind(&tariff)
+	if err != nil {
+		h.log.Error("error while binding:", logger.Error(err))
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		return
+	}
 
+	tariff.Id = c.Param("id")
+	resp, err := h.storage.Tariff().UpdateStaffTarif(&tariff)
+	if err != nil {
+		h.log.Error("error Tariff Update:", logger.Error(err))
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Tariff successfully updated", "id": resp})
 }
 
 func (h *Handler) DeleteStaffTarif(c *gin.Context) {
+	id := c.Param("id")
 
+	resp, err := h.storage.Tariff().DeleteStaffTarif(&models.IdRequest{Id: id})
+	if err != nil {
+		h.log.Error("error deleting tarif:", logger.Error(err))
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "tariff successfully deleted", "id": resp})
 }
--- month_2/lesson_18(mentor)/api/main.go
+++ month_2/lesson_18(mentor)/api/main.go
@@ -16,5 +16,10 @@ func NewServer(h *handler.Handler) *gin.Engine {
 	r.PUT("/branch/:id", h.UpdateBranch)
 	r.DELETE("/branch/:id", h.DeleteBranch)
 
+	r.POST("/tariff", h.CreateStaffTarif)
+	r.GET("/tariff/:id", h.GetStaffTarif)
+	r.GET("/tariff", h.GetAllStaffTarif)
+	r.PUT("/tariff/:id", h.UpdateStaffTarif)
+	r.DELETE("/tariff/:id", h.DeleteStaffTarif)
 	return r
 }
