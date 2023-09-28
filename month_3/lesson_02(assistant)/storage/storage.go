45f8aeea73fb2624c892af386b77cd2ba4224510
jaloldinov <omadbekclever2@gmail.com>
2023-09-28 18:07:09 +0500

    finished crud

---
+++ month_3/lesson_02(assistant)/storage/storage.go
@@ -0,0 +1,18 @@
+package storage
+
+import (
+	"auth/models"
+	"context"
+)
+
+type StorageI interface {
+	User() UsersI
+}
+
+type UsersI interface {
+	CreateUser(context.Context, *models.CreateUser) (string, error)
+	GetUser(context.Context, *models.IdRequest) (*models.User, error)
+	GetAllUser(context.Context, *models.GetAllUserRequest) (*models.GetAllUser, error)
+	UpdateUser(context.Context, *models.User) (string, error)
+	DeleteUser(context.Context, *models.IdRequest) (string, error)
+}
